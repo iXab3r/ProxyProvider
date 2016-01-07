69e9f38f119d80115c546028b5b9a51888db4adc
Xab3r <mail.xaber@gmail.com>
2016-01-07 13:21:15 +0300

    Initial commit

---
+++ Sources/ProxyProvider/ProxiesSource/RawList/RawProxiesSource.cs
@@ -0,0 +1,17 @@
+﻿namespace ProxyProvider.ProxiesSource.RawList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+
+    internal sealed class RawProxiesSource : IProxiesSource
+    {
+        private readonly string[] proxiesRaw = { };
+
+        public IEnumerable<IWebProxy> GetProxies()
+        {
+            return proxiesRaw.Select(x => new WrappedProxy(new Uri($"http://{x}", UriKind.RelativeOrAbsolute)));
+        }
+    }
+}
